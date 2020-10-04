acced0def773d5104c94339556d66aba025a1493
Nitrofen <nitrofen@gmail.com>
2020-10-04 15:27:00 -0500

    sdf

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,13 @@ function toggleMenu() {
     //check the src attribute
     if (hamburgerIcon.src == 'http://' + window.location.hostname + ':' +location.port + '/images/hamburgerIcon.png') {
         // replace the src of the image with an X image
-        hamburgerIcon.src = '/images/Xicon.png';
+        hamburgerIcon.src = 'images/Xicon.png';
         hamburgerIcon.style.filter = "invert(1)";
         mobileNav.style.display = 'block';
         
     } else {
         // replace the src of the image with an X image
-        hamburgerIcon.src = '/images/hamburgerIcon.png';
+        hamburgerIcon.src = 'images/hamburgerIcon.png';
         hamburgerIcon.style.filter = "invert(0)";
         mobileNav.style.display = 'none';
     }
