5c11ad0ed1aa013ea44df5c20886a09c3841b866
Nitrofen <nitrofen@gmail.com>
2020-10-04 15:44:14 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const mobileNav = document.getElementById("sidebar-mobile-nav");
     
 function toggleMenu() {
     console.log(hamburgerIcon.src)
-    console.log('https://' + window.location.hostname + 'images/hamburgerIcon.png')
+    console.log('https://' + window.location.hostname + '/MyLife/' + 'images/hamburgerIcon.png')
     console.log(window.location.hostname);
     console.log(window.location.pathname);
     //check the src attribute
-    if (hamburgerIcon.src == 'https://' + window.location.hostname + 'images/hamburgerIcon.png') {
+    if (hamburgerIcon.src == 'https://' + window.location.hostname + '/MyLife/' + 'images/hamburgerIcon.png') {
         // replace the src of the image with an X image
         hamburgerIcon.src = 'images/Xicon.png';
         hamburgerIcon.style.filter = "invert(1)";
