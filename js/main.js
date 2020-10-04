d1d0f09b899f99d3d545c6a2db89be2327b2097a
Nitrofen <nitrofen@gmail.com>
2020-10-04 15:16:30 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const hamburgerIcon = document.getElementById("hamburgerIcon");
+const desktopNAV = document.getElementById("sidebar-desktop-nav");
+const mobileNav = document.getElementById("sidebar-mobile-nav");
+window.onresize = setHeight;
+    
+function toggleMenu() {
+    //check the src attribute
+    if (hamburgerIcon.src == 'http://' + window.location.hostname + ':' +location.port + '/images/hamburgerIcon.png') {
+        // replace the src of the image with an X image
+        hamburgerIcon.src = '/images/Xicon.png';
+        hamburgerIcon.style.filter = "invert(1)";
+        mobileNav.style.display = 'block';
+        
+    } else {
+        // replace the src of the image with an X image
+        hamburgerIcon.src = '/images/hamburgerIcon.png';
+        hamburgerIcon.style.filter = "invert(0)";
+        mobileNav.style.display = 'none';
+    }
+}
+
+function setHeight() {
+    console.log('hit');
+    desktopNAV.style.height = "100%";
+    desktopNAV.style.height = "100vh";
+    mobileNav.style.height = "100%";
+    mobileNav.style.height = "100vh";
+}
